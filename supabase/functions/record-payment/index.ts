47d712dc3db582b4213ad5e373e27837b4c4d37a
ravgateway <ravgateway@gmail.com>
2025-11-29 01:26:12 -0800

    Minor commit

--- supabase/functions/record-payment/index.ts
+++ supabase/functions/record-payment/index.ts
@@ -1,7 +1,7 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.43.4'
 
 const corsHeaders = {
-  'Access-Control-Allow-Origin': 'http://ravgateway.com',
+  'Access-Control-Allow-Origin': 'https://ravgateway.com',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
   'Access-Control-Allow-Methods': 'POST, OPTIONS',
 }
